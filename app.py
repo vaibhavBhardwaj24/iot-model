8af9647f366dd22b2882c14e0d515c3291c68b77
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-09-05 12:44:36 +0530

    minor fix

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
+import json
 import os
 from flask import Flask, Response, jsonify, request
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+import numpy as np
 import gzip
 from flask_cors import CORS
 import io
-import pytz  
 
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -44,110 +45,207 @@ data = response.data
 df = pd.DataFrame(data)  
 df['createdAt'] = pd.to_datetime(df['createdAt'])
 
-df['coilTemp'] = pd.to_numeric(df['coilTemp'], errors='coerce') + 273.15
-df['surroundingTemp'] = pd.to_numeric(df['surroundingTemp'], errors='coerce') + 273.15
+df['coilTemp_k'] = pd.to_numeric(df['coilTemp'], errors='coerce') + 273.15
+df['surroundingTemp_k'] = pd.to_numeric(df['surroundingTemp'], errors='coerce') + 273.15
 
-df['surroundingTemp'] = df['surroundingTemp'] + 273.15
-df['Relative_COP'] = df['coilTemp']/(df['surroundingTemp'] - df['coilTemp'])
+# df['surroundingTemp'] = df['surroundingTemp'] + 273.15
+df['Relative_COP'] = df['coilTemp_k']/(df['surroundingTemp_k'] - df['coilTemp_k'])
+
 
 @app.route('/graph/week', methods=['POST'])
-def getWeek():
-    fig = go.Figure()
-    df['createdAt'] = pd.to_datetime(df['createdAt'], errors='coerce').dt.tz_convert('UTC')
-    
-    # Get the current time in UTC
-    now = pd.Timestamp.now(tz='UTC')
-    
-    # Calculate the start and end of the week in UTC
-    start_of_week = now - pd.DateOffset(days=now.weekday())
-    end_of_week = start_of_week + pd.DateOffset(days=6)
-    
-    # Filter the DataFrame to include only data from this week
-    df = df[(df['createdAt'] >= start_of_week) & (df['createdAt'] <= end_of_week)]
-    
-    # Set 'createdAt' as the index for correct plotting
-    df.set_index('createdAt', inplace=True)
-    
-    fig.add_trace(go.Scatter(x=df.index, y=df['surroundingTemp'], mode='lines', name='Surrounding Temperature (°C)', yaxis='y1'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['coilTemp'], mode='lines', name='Coil Temperature (°C)', yaxis='y1'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['surroundingHumidity'], mode='lines', name='Surrounding Humidity (%)', yaxis='y2'))
+def get_latest_week_data():
+    df['createdAt'] = pd.to_datetime(df['createdAt'])
+    df['createdAt_naive'] = df['createdAt'].dt.tz_localize(None)
+    current_datetime = np.datetime64('today')
+    one_week_ago = current_datetime - np.timedelta64(7, 'D')
+    latest_week_data = df[df['createdAt_naive'] >= one_week_ago]
+    fig1 = go.Figure()
 
-    fig.update_layout(
+    # Add temperature trace
+    fig1.add_trace(go.Scatter(x=latest_week_data['createdAt'], y=latest_week_data['surroundingTemp'], mode='lines', name='Surrounding Temperature (°C)', yaxis='y1'))
+
+    # Add coil temperature trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=latest_week_data['createdAt'], y=latest_week_data['coilTemp'], mode='lines', name='Coil Temperature (°C)', yaxis='y2'))
+
+    # Add humidity trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=latest_week_data['createdAt'], y=latest_week_data['surroundingHumidity'], mode='lines', name='Surrounding Humidity (%)', yaxis='y2'))
+
+    # Update layout for dual y-axes
+    fig1.update_layout(
         xaxis_title='Time',
-        yaxis=dict(
-            title='Temperature (°C)',
-        ),
+        yaxis_title='Surrounding Temperature (°C)',
         yaxis2=dict(
-            title='Humidity (%)',
+            title='Coil Temperature (°C) / Humidity (%)',
             overlaying='y',
             side='right'
         ),
         legend_title='Variables'
     )
     
-    # Serialize the figure to JSON
-    graph_json = pio.to_json(fig)
-    
-    # Compress JSON data with gzip
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=latest_week_data['createdAt'], y=latest_week_data['Relative_COP'], mode='lines', name='COP'))
+    fig2.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=latest_week_data['coilTemp'], y=latest_week_data['Relative_COP'], mode='lines', name='COP'))
+    fig3.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    graph_json1 = pio.to_json(fig1)
+    graph_json2 = pio.to_json(fig2)
+    graph_json3 = pio.to_json(fig3)
+    combined_json = json.dumps({
+        'coilSurrHumi': graph_json1,
+        'efficencyTime': graph_json2,
+        'efficencyTemp':graph_json3
+    })
+
     buffer = io.BytesIO()
     with gzip.GzipFile(fileobj=buffer, mode='wb') as file:
-        file.write(graph_json.encode('utf-8'))
-    
+        file.write(combined_json.encode('utf-8'))
+
     compressed_json = buffer.getvalue()
-    
-    # Create and return the Flask response
+
     response = Response(compressed_json, content_type='application/json')
     response.headers['Content-Encoding'] = 'gzip'
-    response.headers['Content-Length'] = str(len(compressed_json))
-    
+    response.headers['Content-Length'] = len(compressed_json)
+
     return response
 
+@app.route('/graph/day', methods=['POST'])
+def get_latest_day_data():
+    df['createdAt'] = pd.to_datetime(df['createdAt'])
+    df['createdAt_naive'] = df['createdAt'].dt.tz_localize(None)
+    current_datetime = np.datetime64('today')
+    one_day_ago = current_datetime - np.timedelta64(1, 'D')
+    latest_day_data = df[df['createdAt_naive'] >= one_day_ago]
+    fig1 = go.Figure()
+
+    # Add temperature trace
+    fig1.add_trace(go.Scatter(x=latest_day_data['createdAt'], y=latest_day_data['surroundingTemp'], mode='lines', name='Surrounding Temperature (°C)', yaxis='y1'))
+
+    # Add coil temperature trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=latest_day_data['createdAt'], y=latest_day_data['coilTemp'], mode='lines', name='Coil Temperature (°C)', yaxis='y2'))
+
+    # Add humidity trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=latest_day_data['createdAt'], y=latest_day_data['surroundingHumidity'], mode='lines', name='Surrounding Humidity (%)', yaxis='y2'))
+
+    # Update layout for dual y-axes
+    fig1.update_layout(
+        xaxis_title='Time',
+        yaxis_title='Surrounding Temperature (°C)',
+        yaxis2=dict(
+            title='Coil Temperature (°C) / Humidity (%)',
+            overlaying='y',
+            side='right'
+        ),
+        legend_title='Variables'
+    )
+    
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=latest_day_data['createdAt'], y=latest_day_data['Relative_COP'], mode='lines', name='COP'))
+    fig2.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=latest_day_data['coilTemp'], y=latest_day_data['Relative_COP'], mode='lines', name='COP'))
+    fig3.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    graph_json1 = pio.to_json(fig1)
+    graph_json2 = pio.to_json(fig2)
+    graph_json3 = pio.to_json(fig3)
+    combined_json = json.dumps({
+        'coilSurrHumi': graph_json1,
+        'efficencyTime': graph_json2,
+        'efficencyTemp':graph_json3
+    })
+
+    buffer = io.BytesIO()
+    with gzip.GzipFile(fileobj=buffer, mode='wb') as file:
+        file.write(combined_json.encode('utf-8'))
+
+    compressed_json = buffer.getvalue()
+
+    response = Response(compressed_json, content_type='application/json')
+    response.headers['Content-Encoding'] = 'gzip'
+    response.headers['Content-Length'] = len(compressed_json)
+
+    return response
 
 @app.route('/graph/month', methods=['POST'])
 def get_graph():
-    fig = go.Figure()
-    
-    # Ensure 'createdAt' is in datetime format
-    df['createdAt'] = pd.to_datetime(df['createdAt'])
-    
-    # Set 'createdAt' as the index for correct plotting
-    df.set_index('createdAt', inplace=True)
-    
-    # Add temperature and humidity traces
-    fig.add_trace(go.Scatter(x=df.index, y=df['surroundingTemp'], mode='lines', name='Surrounding Temperature (°C)', yaxis='y1'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['coilTemp'], mode='lines', name='Coil Temperature (°C)', yaxis='y1'))
-    fig.add_trace(go.Scatter(x=df.index, y=df['surroundingHumidity'], mode='lines', name='Surrounding Humidity (%)', yaxis='y2'))
+    # Create first figure
+    fig1 = go.Figure()
+
+    # Add temperature trace
+    fig1.add_trace(go.Scatter(x=df['createdAt'], y=df['surroundingTemp'], mode='lines', name='Surrounding Temperature (°C)', yaxis='y1'))
+
+    # Add coil temperature trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=df['createdAt'], y=df['coilTemp'], mode='lines', name='Coil Temperature (°C)', yaxis='y2'))
+
+    # Add humidity trace with secondary y-axis
+    fig1.add_trace(go.Scatter(x=df['createdAt'], y=df['surroundingHumidity'], mode='lines', name='Surrounding Humidity (%)', yaxis='y2'))
 
     # Update layout for dual y-axes
-    fig.update_layout(
+    fig1.update_layout(
         xaxis_title='Time',
-        yaxis=dict(
-            title='Temperature (°C)',
-        ),
+        yaxis_title='Surrounding Temperature (°C)',
         yaxis2=dict(
-            title='Humidity (%)',
+            title='Coil Temperature (°C) / Humidity (%)',
             overlaying='y',
             side='right'
         ),
         legend_title='Variables'
     )
     
-    # Serialize the figure to JSON
-    graph_json = pio.to_json(fig)
-    
-    # Compress JSON data with gzip
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=df['createdAt'], y=df['Relative_COP'], mode='lines', name='COP'))
+    fig2.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=df['coilTemp'], y=df['Relative_COP'], mode='lines', name='COP'))
+    fig3.update_layout(
+        xaxis_title='Time',
+        yaxis_title='COP',
+        legend_title='Variables'
+    )
+    graph_json1 = pio.to_json(fig1)
+    graph_json2 = pio.to_json(fig2)
+    graph_json3 = pio.to_json(fig3)
+    combined_json = json.dumps({
+        'coilSurrHumi': graph_json1,
+        'efficencyTime': graph_json2,
+        'efficencyTemp':graph_json3
+    })
+
     buffer = io.BytesIO()
     with gzip.GzipFile(fileobj=buffer, mode='wb') as file:
-        file.write(graph_json.encode('utf-8'))
-    
+        file.write(combined_json.encode('utf-8'))
+
     compressed_json = buffer.getvalue()
-    
-    # Create and return the Flask response
+
     response = Response(compressed_json, content_type='application/json')
     response.headers['Content-Encoding'] = 'gzip'
-    response.headers['Content-Length'] = str(len(compressed_json))
-    
+    response.headers['Content-Length'] = len(compressed_json)
+
     return response
+
+# @app.route("/dashboard",methods=['POST'])
+# def dashboard():
+
 
 @app.route('/categories', methods=['POST'])
 def categorize_cop():
@@ -155,6 +253,7 @@ def categorize_cop():
     
     # Access data fields
     iot_id = data.get('iotID')
+    print(iot_id,'qwertyuio')
     q20 = df['Relative_COP'].quantile(0.2)
     q40 = df['Relative_COP'].quantile(0.4)
     q60 = df['Relative_COP'].quantile(0.6)
@@ -168,6 +267,15 @@ def categorize_cop():
     weekly_avg_cop_dict = {date.isoformat(): avg for date, avg in weekly_avg_cop.items()}
 
     row_count = df.shape[0]
+    supabase.table("efficiencyPoint").insert({
+        "iotID": iot_id,
+        "minVal": min_value,
+        "q20": q20,
+        "q40": q40,
+        "q60": q60,
+        "q80": q80,
+        "maxVal": max_value
+    }).execute()
     return jsonify({
         "min_value": min_value,
         "q20": q20,
